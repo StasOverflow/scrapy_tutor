4301da73f8961c47ab9c46231451fedc61d7c129
Vladimir <vladimir.alexeevich.morrowind@gmail.com>
2019-03-13 20:04:49 +0200

    spider reworked

--- tutorial/spiders/habr.py
+++ tutorial/spiders/habr.py
@@ -4,10 +4,20 @@ from tutorial.items import ItemClass
 
 class QuotesSpider(scrapy.Spider):
     name = "habr"
-    start_urls = ("https://habr.com/ru/company/itsumma/blog/443490/", )
+    start_urls = ("https://habr.com/ru/", )
 
     def parse(self, response):
-        # print('RESP IS: ', response)
+        print('RESP IS: ', response)
+        title_link_array = response.xpath('//a[@class="post__title_link"]/@href').extract()
+        for link in title_link_array:
+            yield scrapy.Request(link, self.parse_posts)
+
+    def parse_page(self, response):
+        title_link_array = response.xpath('//a[@class="post__title_link"]/@href').extract()
+        for link in title_link_array:
+            yield scrapy.Request(link, self.parse_posts)
+
+    def parse_posts(self, response):
         title = response.xpath('//span[contains(@class, "post__title")]/text()').get()
         # print('TITLE IS: ', title)
         image = response.xpath('//img[contains(@alt, "banner")]/@src').get()
@@ -17,7 +27,6 @@ class QuotesSpider(scrapy.Spider):
         fields_item['image'] = image
         fields_item['title'] = title
         fields_item['descr'] = description
-
         return fields_item
 
 
